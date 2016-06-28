b953655ba571173f58633442117560539a420fc4
san2ride <sanchezjason@me.com>
2016-06-28 15:38:51 -0600

    Round 2 - Geo Code implemented

--- SityApp/SityApp/SityTableViewController.swift
+++ SityApp/SityApp/SityTableViewController.swift
@@ -7,10 +7,12 @@
 //
 
 import UIKit
+import CoreLocation
 
 class SityTableViewController: UITableViewController {
 
     var citysArray = [City]()
+    var currentCity: City?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,15 +23,86 @@ class SityTableViewController: UITableViewController {
     
     @IBAction func addButton(sender: AnyObject) {
         
+        let alert = UIAlertController(title: "Add Name", message: "Add a new name", preferredStyle: .Alert)
         
-        let c = City()
-        c.zipcode = "Park City, 84068"
-        self.citysArray.append(c)
+        // Create an action called "Save"
         
-        self.tableView.reloadData()
+        let saveAction = UIAlertAction(title: "Save",
+                                       style: .Default,
+                                       handler: {
+                                        (action) in
+                                        
+                                        self.currentCity = City()
+                                        
+                                        if let textField = alert.textFields?.first {
+                                            
+                                            if let name = textField.text {
+                                                print(name)
+                                                
+                                                self.currentCity?.name = name
+                                            }
+                                        }
+                                        
+                                        if let zipTextField = alert.textFields? [1] {
+                                            
+                                            if let zipcode = zipTextField.text {
+                                                   print(zipcode)
+                                                
+                                                self.currentCity?.zipcode = zipcode
+                                                
+                                               
+                                                self.geocoding(zipcode, completion: {
+                                                    (latitude, longitude) in
+                                                    
+                                                    self.currentCity?.latitude = latitude
+                                                    self.currentCity?.longitude = longitude
+                                                    
+                                                    print(latitude)
+                                                    print(longitude)
+                                                    
+                                                    if let current = self.currentCity {
+                                                        self.citysArray.append(current)
+                                                    }
+                                                    
+                                                
+                                                })
+                                            
+                                                
+                                            }
+                                        }
+                                        
+                                        
+                                        
+        })
         
+        // Add the action to the alert instance
+        alert.addAction(saveAction)
+        
+        // Create an action called Cancel
+        
+        let cancelAction = UIAlertAction(title: "Cancel",
+                                         style: .Default) {
+                                            (action) in
+                                            
+        }
+        
+        // Add the cancel action
+        alert.addAction(cancelAction)
+        
+        
+        // Add a textfield to the AlertController
+        
+        alert.addTextFieldWithConfigurationHandler {
+            (textField) in
+            
+            // configure the placeholder text
+            textField.placeholder = "Please enter your name"
+        }
+        
+        // Present the alert using presentViewController
+        self.presentViewController(alert, animated: true, completion: nil)
     }
-    
+        
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.citysArray.count
     }
@@ -48,13 +121,22 @@ class SityTableViewController: UITableViewController {
     
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         
-        
-        
-        
-        
-        
     }
     
+    func geocoding(location: String, completion: (Double, Double) -> ()) {
+        CLGeocoder().geocodeAddressString(location) {
+            
+            (placemarks, error) in
+            
+            if placemarks?.count > 0 {
+                let placemark = placemarks?[0]
+                let location = placemark!.location
+                let coordinate = location?.coordinate
+                completion((coordinate?.latitude)!, (coordinate?.longitude)!)
+            }
+        }
+    }
+
     
     
     
